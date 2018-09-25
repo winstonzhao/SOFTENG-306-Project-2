0341f11aee463f14b12a582bee13a219feeac70a
Reuben Rakete <reuben@rakete.co.nz>
2018-09-25 12:51:47 +1200

    Added scripts to create draggable and droppable objects

---
+++ SOFTENG-306-Project-2/Assets/Scripts/Draggable/SquareResizer.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SquareResizer : MonoBehaviour
+{
+
+    [SerializeField]
+    private float width;
+    public float Width { get { return width; } }
+
+    // Use this for initialization
+    void Start()
+    {
+        UpdateSize();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void UpdateSize()
+    {
+        var text = transform.GetChild(1);
+        var square = transform.GetChild(0);
+
+        width = text.GetComponent<MeshRenderer>().bounds.size.x;
+        square.transform.localScale = new Vector3(width, 1, 1);
+
+        GetComponent<BoxCollider2D>().size = new Vector3(width, 1, 1);
+        GetComponent<BoxCollider2D>().isTrigger = true;
+    }
+}
