20dfd450dff1d01fd6faaaa06f4abd6926b3c4bb
Reuben Rakete <reuben@rakete.co.nz>
2018-09-25 12:51:47 +1200

    Added scripts to create draggable and droppable objects

---
+++ SOFTENG-306-Project-2/Assets/Scripts/Draggable/DraggableItem.cs
@@ -0,0 +1,176 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DraggableItem : Draggable
+{
+
+    private bool mouseInside = false;
+    private bool dragging = false;
+    private bool moving = false;
+
+    private IDropZone dropZone;
+
+    private List<IDropZone> newDropZones = new List<IDropZone>();
+
+    private Vector3 target;
+
+    Vector3 prevMousePos;
+
+    private Vector3 homePos;
+    public override Vector3 HomePos
+    {
+        get
+        {
+            return homePos;
+        }
+        set
+        {
+            homePos = value;
+            if (!dragging)
+            {
+                MoveTo(value);
+            }
+        }
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    public void SetDropZone(IDropZone list)
+    {
+        this.dropZone = list;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (dragging)
+        {
+            Vector3 localMousePos = Input.mousePosition;
+            Vector3 mousePosWorld = Camera.main.ScreenToWorldPoint(new Vector3(localMousePos.x, localMousePos.y, Camera.main.nearClipPlane));
+
+            Vector3 diff = mousePosWorld - prevMousePos;
+            transform.Translate(diff);
+
+            if (dropZone != null) 
+            {
+                dropZone.OnItemDrag(this);
+            }
+
+            prevMousePos = mousePosWorld;
+        }
+
+        if (moving)
+        {
+            transform.position = Vector3.Lerp(transform.position, target, 0.1f);
+
+            if (Vector3.Distance(transform.position, target) < 0.01f)
+            {
+                moving = false;
+            }
+        }
+    }
+
+    void OnMouseEnter()
+    {
+        mouseInside = true;
+    }
+
+    void OnMouseLeave()
+    {
+        mouseInside = false;
+    }
+
+    void OnMouseDown()
+    {
+        if (!mouseInside) return;
+        dragging = true;
+        moving = false;
+        Vector3 mousePos = Input.mousePosition;
+        Vector3 mousePosWorld = Camera.main.ScreenToWorldPoint(new Vector3(mousePos.x, mousePos.y, Camera.main.nearClipPlane));
+        prevMousePos = mousePosWorld;
+
+        if (dropZone != null)
+        {
+            dropZone.OnDragStart(this);
+        }
+    }
+
+    void OnMouseUp()
+    {
+        dragging = false;
+        if (newDropZones.Count > 0)
+        {
+            if (dropZone != null) 
+            {
+                dropZone.OnItemRemove(this);
+            }
+            
+            newDropZones[0].OnDrop(this);
+            dropZone = newDropZones[0];
+            newDropZones.Clear();
+        }
+        else
+        {
+            if (dropZone != null) 
+            {
+                dropZone.OnDragFinish(this);
+            }
+        }
+        MoveTo(homePos);
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (!dragging) return;
+        
+        var possibleDropZone = col.GetComponent<IDropZone>();
+        if (possibleDropZone == null) return;
+        
+        foreach(var dropZone in newDropZones)
+        {
+            dropZone.OnDragExit(this);
+        }
+        
+        newDropZones.Insert(0, possibleDropZone);
+        newDropZones[0].OnDragEnter(this);
+    }
+
+    void OnTriggerExit2D(Collider2D col)
+    {
+        if (!dragging) return;
+
+        var possibleDropZone = col.GetComponent<IDropZone>();
+        if (possibleDropZone == null) return;
+        
+        var prevIndex = newDropZones.IndexOf(possibleDropZone);
+        if (prevIndex > -1)
+        {
+            newDropZones[prevIndex].OnDragExit(this);
+            newDropZones.RemoveAt(prevIndex);
+
+            if (prevIndex == 0 && newDropZones.Count > 0)
+            {
+                newDropZones[0].OnDragEnter(this);
+            }
+        }
+        else if (possibleDropZone == dropZone)
+        {
+            dropZone.OnDragExit(this);
+        }
+
+    }
+
+
+    void MoveTo(Vector3 newPos)
+    {
+        if (dropZone == null) return;
+
+        moving = true;
+        target = newPos;
+    }
+}
---
+++ SOFTENG-306-Project-2/Assets/Scripts/Draggable/SingleDropZone.cs
@@ -0,0 +1,58 @@
+using UnityEngine;
+
+[RequireComponent(typeof(BoxCollider2D))]
+[RequireComponent(typeof(Rigidbody2D))]
+public class SingleDropZone : MonoBehaviour, IDropZone
+{
+    private Draggable currentItem;
+
+    public void Start()
+    {
+        var rigidbody = GetComponent<Rigidbody2D>();
+        if (rigidbody == null) 
+        {
+            rigidbody = gameObject.AddComponent<Rigidbody2D>();
+            rigidbody.bodyType = RigidbodyType2D.Kinematic;
+        }
+    }
+
+    public void OnDragEnter(Draggable item)
+    {
+        GetComponent<SpriteRenderer>().color = new Color(1.0f, 0.0f, 0.0f);
+    }
+
+    public void OnDragExit(Draggable item)
+    {
+        GetComponent<SpriteRenderer>().color = new Color(1.0f, 1.0f, 1.0f);
+    }
+
+    public void OnDragFinish(Draggable item)
+    {
+        if (!item.GetComponent<Collider2D>().IsTouching(GetComponent<Collider2D>()))
+        {
+            item.GetComponent<DraggableItem>().SetDropZone(null);
+        }
+    }
+
+    public void OnDragStart(Draggable item)
+    {
+        
+    }
+
+    public void OnDrop(Draggable item)
+    {
+        currentItem = item;
+        currentItem.HomePos = transform.position;
+        GetComponent<SpriteRenderer>().color = new Color(1.0f, 1.0f, 1.0f);
+    }
+
+    public void OnItemDrag(Draggable item)
+    {
+        
+    }
+
+    public void OnItemRemove(Draggable item)
+    {
+        currentItem = null;
+    }
+}
