ea22cf0ba9a75d6b623b803d7e91fc130b04264e
Winston Zhao <winstonzhao@outlook.co.nz>
2018-09-27 01:04:46 +1200

    Added GameManager and Player Model Template
    - Added Images for a Simple Menu Screen
    - Simple Menu Screen
    - Setup the Singleton GameManager

---
+++ SOFTENG-306-Project-2/Assets/Scripts/GameManager.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+	public static GameManager instance;
+	private Player _player;
+
+	
+	private void Awake()
+	{
+		Debug.Log("gameManager Alive");
+		//Check if instance already exists
+		if (instance == null)
+                
+			//if not, set instance to this
+			instance = this;
+            
+		//If instance already exists and it's not this:
+		else if (instance != this)
+                
+			//Then destroy this. This enforces our singleton pattern, meaning there can only ever be one instance of a GameManager.
+			Destroy(gameObject);    
+            
+		//Sets this to not be destroyed when reloading scene
+		DontDestroyOnLoad(gameObject);
+	}
+
+	public void ChangeScene(string levelName)
+	{
+		SceneManager.LoadScene(levelName);
+	}
+
+	public void QuitGame()
+	{
+		Application.Quit();
+	}
+
+	public Player Player
+	{
+		get { return _player; }
+		set { _player = value; }
+	}
+}
