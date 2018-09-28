4e8478633a7c7145156df200122dbc35cc44b380
Simon Su <f-7-beihan@hotmail.com>
2018-09-28 14:05:23 +1200

    Advanced floor generator (#7)
    * Advanced Floor Generator without collison boxes
    
    * invis colliders added
    
    * bug fix on for isotransform
    
    * finished advanced floor generator and fix bugs for creating duplicate gameobjects
    
    * fixed changes

---
+++ SOFTENG-306-Project-2/Assets/Scripts/ObjectBuilderScript.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(BaseFloorGenerator))]
+public class ObjectBuilderEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+
+        BaseFloorGenerator myScript = (BaseFloorGenerator)target;
+        if (GUILayout.Button("Build Object"))
+        {
+            myScript.RePaint();
+        }
+    }
+}
