f12215dcc60e6e452134924eb2d1e601af92908c
Tina Chen <tche278@aucklanduni.ac.nz>
2018-09-29 16:36:37 +1200

    Add tutorial and change scene script

---
+++ SOFTENG-306-Project-2/Assets/Scripts/ElectricalScripts/ElectricalTutorial.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class ElectricalTutorial : MonoBehaviour {
+
+    //Make sure to attach these Buttons in the Inspector
+    public Button start;
+    public string path;
+
+    void Start()
+    {
+
+        start = start.GetComponent<Button>();
+
+        start.onClick.AddListener(TaskOnClick);
+    }
+
+    void TaskOnClick()
+    {
+        SceneManager.LoadScene(path);
+    }
+}
