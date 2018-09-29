c1ca54b49998d3eedce40112927d4497b9fd0596
SheepySean <sold940@aucklanduni.ac.nz>
2018-09-30 12:02:08 +1300

    Add ultimate isometric toolkit ffrom Asset Store

--- SOFTENG-306-Project-2/Assets/Ultimate Isometric Toolkit/Scripts/External/ExposePropertyAttribute.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-namespace Ultimate_Isometric_Toolkit.Scripts.External
-{
-    [AttributeUsage(AttributeTargets.Property)]
-    public class ExposePropertyAttribute : Attribute
-    {
-    }
-}
