abf64734a82055d06c8ba145c04d07aa4c279c18
Franklin Wang <fractalemagic@gmail.com>
2018-09-30 04:51:27 +1300

    Prefer updating existing player objects rather than recreating

--- SOFTENG-306-Project-2/Assets/Scripts/Multiplayer/MultiplayerController.cs
+++ SOFTENG-306-Project-2/Assets/Scripts/Multiplayer/MultiplayerController.cs
@@ -17,13 +17,30 @@ namespace Multiplayer
 
         private Player MyPlayer;
 
+        private GameObject PlayerPrefab;
+
         private float LastSyncAt;
 
         private Dictionary<string, Player> Players;
 
+        /// <summary>
+        /// Whether the <see cref="Players"/> object has changed
+        /// </summary>
+        private bool IsPlayersDirty;
+
+        /// <summary>
+        ///  Positive when there are more new players compared to the previous tick
+        /// </summary>
+        private int PlayerCountChange;
+
         public void Register(Player player)
         {
             MyPlayer = player;
+        }
+
+        private void Awake()
+        {
+            PlayerPrefab = AssetDatabase.LoadAssetAtPath<GameObject>("Assets/Prefabs/Multiplayer.prefab");
         }
 
         private void Start()
@@ -82,7 +99,10 @@ namespace Multiplayer
                 players[player.username] = player;
             }
 
+            var prevCount = Players == null ? 0 : Players.Count;
+            PlayerCountChange = players.Count - prevCount;
             Players = players;
+            IsPlayersDirty = true;
         }
 
         private void Update()
@@ -107,6 +127,13 @@ namespace Multiplayer
 
         private void UpdatePlayers()
         {
+            if (!IsPlayersDirty)
+            {
+                return;
+            }
+
+            IsPlayersDirty = false;
+
             var players = Players;
 
             if (players == null)
@@ -114,15 +141,7 @@ namespace Multiplayer
                 return;
             }
 
-            while (transform.childCount != 0)
-            {
-                foreach (Transform child in transform)
-                {
-                    DestroyImmediate(child.gameObject);
-                }
-            }
-
-            var prefab = AssetDatabase.LoadAssetAtPath<GameObject>("Assets/Prefabs/Multiplayer.prefab");
+            var hasNewPlayer = false;
 
             foreach (var pair in players)
             {
@@ -133,9 +152,34 @@ namespace Multiplayer
                     continue;
                 }
 
-                var gameObject = PrefabUtility.InstantiatePrefab(prefab) as GameObject;
-                gameObject.transform.parent = transform;
-                gameObject.GetComponent<PlayerController>().SetPlayer(player);
+                var objectName = "Player " + player.username;
+
+                var find = transform.Find(objectName);
+                GameObject playerObject = find != null ? find.gameObject : null;
+
+                if (playerObject == null)
+                {
+                    playerObject = PrefabUtility.InstantiatePrefab(PlayerPrefab) as GameObject;
+                    playerObject.name = objectName;
+                    playerObject.transform.parent = transform;
+                    hasNewPlayer = true;
+                }
+
+                playerObject.GetComponent<PlayerController>().SetPlayer(player);
+            }
+
+            // Remove orphan player objects i.e. players that don't exist on the server anymore
+            // Check for new player in case someone joined as someone else left i.e. player count didn't change
+            if (PlayerCountChange != 0 || hasNewPlayer)
+            {
+                foreach (Transform child in transform)
+                {
+                    var username = child.GetComponent<PlayerController>().Username;
+                    if (!players.ContainsKey(username))
+                    {
+                        DestroyImmediate(child.gameObject);
+                    }
+                }
             }
         }
     }
