f5a98159f0960e1d85714d9894a0ecd4020ae8fd
Carl-Tang <ytan415@aucklanduni.ac.nz>
2018-09-30 14:54:30 +1300

    Add support for placement rules for tiles
    Placement rules such as bridges only been able to be placed on water, and roads not been able to be placed on water, etc.
    Pair programmed with Sean.

---
+++ SOFTENG-306-Project-2/Assets/Scripts/Draggable/DraggableIsoItem.cs
@@ -0,0 +1,208 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Ultimate_Isometric_Toolkit.Scripts.Core;
+
+public class DraggableIsoItem : Draggable
+{
+
+    public string name;
+
+    private bool mouseInside = false;
+    private bool dragging = false;
+    private bool moving = false;
+
+    private IsoDropZone dropZone;
+
+    private List<IsoDropZone> newDropZones = new List<IsoDropZone>();
+
+    private Vector3 target;
+
+    Vector3 prevMousePos;
+
+    public Vector3 homePos;
+    public override Vector3 HomePos
+    {
+        get
+        {
+            return homePos;
+        }
+        set
+        {
+            homePos = value;
+            if (!dragging)
+            {
+                MoveTo(value);
+            }
+        }
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    public void SetDropZone(IsoDropZone list)
+    {
+        this.dropZone = list;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+
+        //Debug.Log("mouse at (iso): ");
+        //Debug.Log(Ultimate_Isometric_Toolkit.Scripts.Utils.Isometric.ScreenToIso(Input.mousePosition));
+
+        if (dragging)
+        {
+            Vector3 localMousePos = Input.mousePosition;
+            Vector3 mousePosWorld = Camera.main.ScreenToWorldPoint(new Vector3(localMousePos.x, localMousePos.y, Camera.main.nearClipPlane));
+
+            Vector3 diff = mousePosWorld - prevMousePos;
+            //transform.Translate(diff);
+            //Debug.Log("mouse at (iso): ");
+            //Debug.Log(Ultimate_Isometric_Toolkit.Scripts.Utils.Isometric.ScreenToIso(Input.mousePosition).ToString());
+            GetComponent<IsoTransform>().transform.Translate(diff);
+
+            if (dropZone != null)
+            {
+                dropZone.OnItemDrag(this);
+            }
+
+            prevMousePos = mousePosWorld;
+        }
+
+
+        if (moving)
+        {
+            transform.position = Vector3.Lerp(transform.position, target, 0.1f);
+            //GetComponent<Ultimate_Isometric_Toolkit.Scripts.Core.IsoTransform>().transform.position = Vector3.Lerp(transform.position, target, 0.1f);
+
+            if (Vector3.Distance(transform.position, target) < 0.01f)
+            {
+                moving = false;
+            }
+        }
+    }
+
+    void OnMouseEnter()
+    {
+        //Debug.Log("mouse enter");
+        mouseInside = true;
+    }
+
+    void OnMouseLeave()
+    {
+        //Debug.Log("mouse leave");
+        mouseInside = false;
+    }
+
+    void OnMouseDown()
+    {
+        if (!mouseInside) return;
+        dragging = true;
+        moving = false;
+        GetComponent<SpriteRenderer>().sortingOrder = 1;
+        Vector3 mousePos = Input.mousePosition;
+        Vector3 mousePosWorld = Camera.main.ScreenToWorldPoint(new Vector3(mousePos.x, mousePos.y, Camera.main.nearClipPlane));
+        prevMousePos = mousePosWorld;
+
+        //Debug.Log("mouse clicked");
+
+        if (dropZone != null)
+        {
+            dropZone.OnDragStart(this);
+        }
+    }
+
+    void OnMouseUp()
+    {
+        dragging = false;
+        if (newDropZones.Count > 0 && newDropZones[0].droppableNames.Contains(this.name))
+        {
+            Debug.Log("entered 1st if statement");
+            if (dropZone != null)
+            {
+                dropZone.OnItemRemove(this);
+            }
+
+            newDropZones[0].OnDrop(this);
+            dropZone = newDropZones[0];
+            newDropZones.Clear();
+        }
+        else if (newDropZones.Count > 0 && !newDropZones[0].droppableNames.Contains(this.name))
+        {
+            newDropZones[0].OnDragExit(this);
+            Debug.Log("entered 2nd if statement");
+            if (dropZone != null)
+            {
+                dropZone.OnDragFinish(this);
+            }
+        }
+        else
+        {
+            Debug.Log("entered else statement");
+            Debug.Log(dropZone);
+            if (dropZone != null)
+            {
+                dropZone.OnDragFinish(this);
+            }
+        }
+        Debug.Log(homePos);
+        MoveTo(homePos);
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        //Debug.Log("trigger enter");
+        if (!dragging) return;
+
+        var possibleDropZone = col.GetComponent<IsoDropZone>();
+        if (possibleDropZone == null) return;
+
+        foreach (var dropZone in newDropZones)
+        {
+            dropZone.OnDragExit(this);
+        }
+
+        newDropZones.Insert(0, possibleDropZone);
+        newDropZones[0].OnDragEnter(this);
+    }
+
+    void OnTriggerExit2D(Collider2D col)
+    {
+        if (!dragging) return;
+
+        var possibleDropZone = col.GetComponent<IsoDropZone>();
+        if (possibleDropZone == null) return;
+
+        var prevIndex = newDropZones.IndexOf(possibleDropZone);
+        if (prevIndex > -1)
+        {
+            newDropZones[prevIndex].OnDragExit(this);
+            newDropZones.RemoveAt(prevIndex);
+
+            if (prevIndex == 0 && newDropZones.Count > 0)
+            {
+                newDropZones[0].OnDragEnter(this);
+            }
+        }
+        else if (possibleDropZone == dropZone)
+        {
+            dropZone.OnDragExit(this);
+        }
+
+    }
+
+
+    public void MoveTo(Vector3 newPos)
+    {
+        if (dropZone == null) return;
+
+        moving = true;
+        target = newPos;
+        GetComponent<SpriteRenderer>().sortingOrder = 0;
+    }
+}
---
+++ SOFTENG-306-Project-2/Assets/Scripts/Draggable/IsoDropZone.cs
@@ -0,0 +1,113 @@
+﻿using UnityEngine;
+using UnityEditor;
+using UltimateIsometricToolkit.physics;
+using System.Collections.Generic;
+
+//[RequireComponent(typeof(IsoCollider))]
+//[RequireComponent(typeof(Rigidbody2D))]
+public class IsoDropZone : MonoBehaviour, IDropZone
+{
+
+    private GameObject child;
+
+    public string prefebName;
+    public List<string> droppableNames;
+
+    private Draggable currentItem;
+
+    public void Start()
+    {
+
+        var rigidbody = GetComponent<Rigidbody2D>();
+        if (rigidbody == null)
+        {
+            rigidbody = gameObject.AddComponent<Rigidbody2D>();
+            rigidbody.bodyType = RigidbodyType2D.Kinematic;
+        }
+
+        createPrefeb();
+    }
+
+    void createPrefeb()
+    {
+        if (prefebName != "")
+        {
+            // instantiate
+            Object prefab = AssetDatabase.LoadAssetAtPath("Assets/Prefabs/" + prefebName + ".prefab", typeof(GameObject));
+            child = PrefabUtility.InstantiatePrefab(prefab) as GameObject;
+            child.GetComponent<DraggableIsoItem>().SetDropZone(this);
+            child.GetComponent<DraggableIsoItem>().homePos = transform.position;
+            Debug.Log(child.GetComponent<DraggableIsoItem>().homePos);
+            if (child)
+            {
+                Debug.Log("new " + prefebName + " created");
+            }
+
+        }
+    }
+
+    void OnMouseEnter()
+    {
+        //Debug.Log("drop zone mouse entered");
+    }
+
+    public void OnDragEnter(Draggable item)
+    {
+        SetDropZoneActive(false);
+        GetComponent<SpriteRenderer>().color = new Color(1.0f, 0.0f, 0.0f);
+    }
+
+    public void OnDragExit(Draggable item)
+    {
+        GetComponent<SpriteRenderer>().color = new Color(1.0f, 1.0f, 1.0f);
+        //SetDropZoneActive(true);
+        Debug.Log("Dragged away");
+
+
+    }
+
+    public void OnDragFinish(Draggable item)
+    {
+        //if (!item.GetComponent<IsoCollider>().GetComponent<Collider>().IsTouching(GetComponent<IsoCollider>()))
+        //{
+        //    // disable if we need the snapback effect
+        //    //item.GetComponent<DraggableItem>().SetDropZone(null);
+        //}
+        SetDropZoneActive(false);
+
+        GetComponent<SpriteRenderer>().sortingLayerName = "BackGround";
+    }
+
+    public void OnDragStart(Draggable item)
+    {
+        GetComponent<SpriteRenderer>().sortingLayerName = "Ground";
+    }
+
+    public void OnDrop(Draggable item)
+    {
+        currentItem = item;
+        currentItem.HomePos = transform.position;
+        GetComponent<SpriteRenderer>().color = new Color(1.0f, 1.0f, 1.0f);
+        GetComponent<SpriteRenderer>().sortingLayerName = "BackGround";
+    }
+
+    public void OnItemDrag(Draggable item)
+    {
+
+    }
+
+    public void OnItemRemove(Draggable item)
+    {
+        currentItem = null;
+        createPrefeb();
+        SetDropZoneActive(true);
+    }
+
+    /*
+     * Enable or disable the drop zone by changing whether or not it can be hit by a ray cast
+     */
+    private void SetDropZoneActive(bool enable)
+    {
+        gameObject.layer = enable ? 1 : 2;
+    }
+}
