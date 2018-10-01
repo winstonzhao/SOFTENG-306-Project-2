042891499a3014498d4423d9da2d96e2c3621f9a
rmberriman <rjl@hotmail.co.nz>
2018-10-01 22:26:13 +1300

    Gate info popup
    Made a popup for the and gate on level 1. Cannot get the same to popup for the alien

---
+++ SOFTENG-306-Project-2/Assets/HideHelp.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HideHelp : MonoBehaviour {
+
+    //Make sure to attach these Buttons in the Inspector
+    public Button start;
+    public Canvas helpDialog;
+
+    // Use this for initialization
+    void Start()
+    {
+        start = start.GetComponent<Button>();
+
+        start.onClick.AddListener(TaskOnClick);
+    }
+
+    void TaskOnClick()
+    {
+        helpDialog.enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
