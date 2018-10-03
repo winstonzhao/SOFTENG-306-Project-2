bf0f69707838a68c9422521ba3420a97a9815971
Franklin Wang <fractalemagic@gmail.com>
2018-10-04 05:31:30 +1300

    Basic hiscores

---
+++ SOFTENG-306-Project-2/Assets/Scripts/Game/Hiscores/UserInterface/HiscoreView.cs
@@ -0,0 +1,65 @@
+using System;
+using System.Collections;
+using Ultimate_Isometric_Toolkit.Scripts.Core;
+using UnityEngine;
+using UnityEngine.Animations;
+using UnityEngine.UI;
+
+namespace Game.Hiscores.UserInterface
+{
+    public class HiscoreView : MonoBehaviour
+    {
+        private GameObject ScorePrefab;
+
+        private void Start()
+        {
+            ScorePrefab = Resources.Load<GameObject>("Prefabs/Hiscores/Score");
+
+            var controller = FindObjectOfType<HiscoreController>();
+
+            controller.OnMinigameChange += (s, e) => { StartCoroutine("Display", controller.Minigame); };
+
+            StartCoroutine("Display", controller.Minigame);
+        }
+
+        private IEnumerator Display(Minigames minigame)
+        {
+            while (transform.childCount != 0)
+            {
+                foreach (Transform child in transform)
+                {
+                    DestroyImmediate(child.gameObject);
+                }
+
+                yield return null;
+            }
+
+            var scores = Toolbox.Instance.Hiscores.Get(minigame);
+
+            var y = 0.0f;
+
+            foreach (var score in scores)
+            {
+                var view = Instantiate(ScorePrefab);
+
+                view.transform.Find("Name").GetComponent<Text>().text = score.Minigame.ToString();
+                view.transform.Find("Date").GetComponent<Text>().text = score.CreatedAt.ToShortDateString();
+                view.transform.Find("Score").GetComponent<Text>().text = "Scored " + score.Value;
+
+                view.transform.SetParent(transform);
+
+                var rt = view.GetComponent<RectTransform>();
+
+                rt.SetInsetAndSizeFromParentEdge(RectTransform.Edge.Top, y, rt.sizeDelta.y);
+
+                var offsetMax = rt.offsetMax;
+                offsetMax.x = 0;
+                rt.offsetMax = offsetMax;
+
+                y += rt.sizeDelta.y;
+
+                yield return null;
+            }
+        }
+    }
+}
