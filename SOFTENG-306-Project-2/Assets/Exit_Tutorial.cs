66c5cda5af93e98530007bae16713af543779c7b
rmberriman <rjl@hotmail.co.nz>
2018-10-11 11:00:07 +1300

    In-Game Tutorial
    Added the in-game logic gate tutorial
    
    Co-Authored-By: twchen97 <twchen97@users.noreply.github.com>

---
+++ SOFTENG-306-Project-2/Assets/Exit_Tutorial.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Exit_Tutorial : MonoBehaviour {
+
+    public Button exit;
+    public Canvas backgroundCanvas;
+    public Canvas screen1;
+    public Canvas screen2;
+    public Canvas screen3;
+    public Canvas screen4;
+    public Canvas screen5;
+
+    void Start()
+    {
+        exit = exit.GetComponent<Button>();
+        exit.onClick.AddListener(TaskOnClick);
+    }
+
+    void TaskOnClick()
+    {
+        backgroundCanvas.enabled = false;
+        screen1.enabled = false;
+        screen2.enabled = false;
+        screen3.enabled = false;
+        screen4.enabled = false;
+        screen5.enabled = false;
+    }
+}
---
+++ SOFTENG-306-Project-2/Assets/Show_Tutorial.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Show_Tutorial : MonoBehaviour {
+
+    public Button button;
+    public Canvas firstCanvas;
+    public Canvas secondCanvas;
+
+    void Start()
+    {
+        button = button.GetComponent<Button>();
+        button.onClick.AddListener(TaskOnClick);
+    }
+
+    void TaskOnClick()
+    {
+        firstCanvas.enabled = !firstCanvas.enabled;
+        secondCanvas.enabled = !secondCanvas.enabled;
+    }
+}
