0b041ba9f408456410907a92c2e6aab9f28c6488
rmberriman <rjl@hotmail.co.nz>
2018-10-10 22:19:23 +1300

    Added Confirm Close
    Cleaned up level 1

---
+++ SOFTENG-306-Project-2/Assets/Scripts/ElectricalScripts/Click_Dialog.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Click_Dialog : MonoBehaviour {
+
+    public Button start;
+    public Canvas helpCanvas;
+
+    void Start()
+    {
+        start = start.GetComponent<Button>();
+        start.onClick.AddListener(TaskOnClick);
+    }
+
+    void TaskOnClick()
+    {
+        helpCanvas.enabled = true;
+    }
+}
