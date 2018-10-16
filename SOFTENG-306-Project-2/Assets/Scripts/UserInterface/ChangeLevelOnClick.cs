6e59555daa8ecd7fa2e34641ba3ca95341566a9d
Winston Zhao <winstonzhao@outlook.co.nz>
2018-10-17 05:17:45 +1300

    Created Name Change Scene and Polishing UI

---
+++ SOFTENG-306-Project-2/Assets/Scripts/UserInterface/ChangeLevelOnClick.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ChangeLevelOnClick : MonoBehaviour {
+
+    public string LevelName;
+
+	// Use this for initialization
+	void Start () {
+        GetComponent<Button>().onClick.AddListener(() =>
+        {
+            Toolbox.Instance.GameManager.ChangeScene(LevelName);
+        });
+	}
+	
+	
+}
