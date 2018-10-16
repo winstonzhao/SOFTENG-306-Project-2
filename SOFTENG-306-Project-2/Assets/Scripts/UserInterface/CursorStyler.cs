42e041cbfdedf515a53ab3a34ff1a62eea55bcf8
Tina Chen <tche278@aucklanduni.ac.nz>
2018-10-17 11:27:18 +1300

    Add cursor handler and stylers to Level 1

---
+++ SOFTENG-306-Project-2/Assets/Scripts/UserInterface/CursorStyler.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+/**
+ * Hook class for cursor handlers, attach this to every component that you wish the cursor change on
+ */
+public class CursorStyler : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    public bool enabled = true;
+
+    private CursorHandler handler;
+
+    /**
+     * Set the default cursor handler
+     */
+    void Awake()
+    {
+        handler = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<CursorHandler>();
+    }
+
+
+    /**
+     * Mouse enter for GUI elements
+     */
+    public void OnPointerEnter(PointerEventData pointerEventData)
+    {
+        if (enabled)
+        {
+            handler.CursorEnter();
+        }
+    }
+
+    /**
+     * Mouse exit for GUI elements
+     */
+    public void OnPointerExit(PointerEventData pointerEventData)
+    {
+        if (enabled)
+        {
+            handler.CursorClear();
+        }
+    }
+
+
+    /**
+     * =========================================
+     * For GameObjects
+     * =========================================
+     */
+
+
+    void OnMouseEnter()
+    {
+        if (!EventSystem.current.IsPointerOverGameObject() && enabled) // Block mouse clicks through a canvas
+        {
+            handler.CursorEnter();
+        }
+    }
+
+    private void OnMouseExit()
+    {
+        if (!EventSystem.current.IsPointerOverGameObject() && enabled)
+        {
+            handler.CursorClear();
+        }
+    }
+
+    private void OnMouseUp()
+    {
+        handler.CursorClear();
+    }
+
+    private void OnMouseDrag()
+    {
+        if (!EventSystem.current.IsPointerOverGameObject() && enabled)
+        {
+            handler.CursorDrag();
+        }
+    }
+
+}
