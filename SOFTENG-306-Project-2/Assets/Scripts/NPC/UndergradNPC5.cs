9825ece481a0a6885f1371c9110ee8a0bcf2c749
SheepySean <sold940@aucklanduni.ac.nz>
2018-10-18 11:48:19 +1300

    Add undergrad NPCs "Hannah" and "Sophie"

---
+++ SOFTENG-306-Project-2/Assets/Scripts/NPC/UndergradNPC5.cs
@@ -0,0 +1,107 @@
+﻿using System.Collections.Generic;
+using GameDialog;
+
+public class UndergradNPC5 : NPC
+{
+    // Use this for initialization
+    public override Dialog GetDialog()
+    {
+        var me = Toolbox.Instance.GameManager.Player.Username;
+
+        const string npc = "Hannah";
+
+        var frame = new DialogFrame(me, "Hello, I'm " + me)
+        {
+            Next = new DialogFrame(npc, "Nice to meet you!")
+            {
+                Next = new DialogFrame(npc, "My name's " + npc + ", and I study engineering science!")
+                {
+                    Next = new DialogFrame(me, "Hey, is it ok if I ask you a couple of questions?")
+                    {
+                        Options = new Dictionary<string, DialogFrame>
+                        {
+                            {
+                                "Why Engineering?", new DialogFrame(npc,
+                                    "I chose engineering as I have always loved using maths and" +
+                                    " science to solve problems.")
+                                {
+                                    Next = new DialogFrame(npc,
+                                        "Being curious about why things work the way they do also made" +
+                                        " me interested in engineering as a career choice, as through engineering" +
+                                        " I could get a better understanding of the world around me.")
+                                    {
+                                        Next = new DialogFrame(me, "Wow that's so cool!")
+                                        {
+                                            Next = new DialogFrame(npc,
+                                                "It really is. Come back and talk to me anytime.")
+                                        }
+                                    }
+                                }
+                            },
+                            {
+                                "Your experience?", new DialogFrame(npc,
+                                    "My biggest concern coming in was not meeting new people. " +
+                                    "I thought with engineering being an academic heavy subject, " +
+                                    "and the fact that people joke about engineers being anti-social, " +
+                                    "I wouldn’t be able to find many new people to make friends with.")
+                                {
+                                    Next = new DialogFrame(npc,
+                                        "Everyone is really lovely though, and there is a lot of group work " +
+                                        "where you get to meet new people.")
+                                    {
+                                        Next = new DialogFrame(me,
+                                            "Oh wow, that's really interesting!")
+                                        {
+                                            Next = new DialogFrame(npc,
+                                                "Thanks, no problem, come to talk me anytime!")
+                                        }                                
+                                    }
+                                }
+                            },
+                            {
+                                "Any advice?", new DialogFrame(npc,
+                                    "I would advise that you go into university with an idea of meeting new people. " +
+                                    "If you go in with a lot of friends you are already close to, that’s great! ")
+                                {
+                                    Next = new DialogFrame(npc,
+                                        "But having new friends who study the same thing as you can really help " +
+                                        "encourage you to get through hard assignments and tests.")
+                                    {
+                                        Next = new DialogFrame(npc,
+                                            "Plus creating a good network of contacts early on is only going to help you further on in life and in your career!")
+                                        {
+                                            Next = new DialogFrame(me, "Thank you, that's really helpful!")
+                                            {
+                                                Next = new DialogFrame(npc,
+                                                    "Thanks, no problem, come to talk me anytime!")
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        };
+
+        var directions = new Dictionary<string, DialogPosition>
+        {
+            { me, DialogPosition.Left }, { npc, DialogPosition.Right }
+        };
+
+        return new Dialog(frame, directions, () =>
+        {
+            Toolbox.Instance.QuestManager.MarkCurrentFinished("networking");
+            Toolbox.Instance.AchievementsManager.MarkCompleted("speak-undergrad");
+        });
+    }
+
+    public override void Update()
+    {
+        var networking = Toolbox.Instance.QuestManager.HasFinishedOrIsCurrent("networking");
+        gameObject.SetActive(networking);
+
+        base.Update();
+    }
+}
---
+++ SOFTENG-306-Project-2/Assets/Scripts/NPC/UndergradNPC6.cs
@@ -0,0 +1,101 @@
+﻿using System.Collections.Generic;
+using GameDialog;
+
+public class UndergradNPC6 : NPC
+{
+    // Use this for initialization
+    public override Dialog GetDialog()
+    {
+        var me = Toolbox.Instance.GameManager.Player.Username;
+
+        const string npc = "Sophie";
+
+        var frame = new DialogFrame(me, "Hello, I'm " + me)
+        {
+            Next = new DialogFrame(npc, "Nice to meet you!")
+            {
+                Next = new DialogFrame(npc, "My name's " + npc + ", and I study biomedical engineering!")
+                {
+                    Next = new DialogFrame(me, "Is it alright if I ask you some questions?")
+                    {
+                        Options = new Dictionary<string, DialogFrame>
+                        {
+                            {
+                                "Why Engineering?", new DialogFrame(npc,
+                                    "I loved graphics, physics and biology in high school " +
+                                    "and engineering was the best mix. ")
+                                {
+                                    Next = new DialogFrame(npc,
+                                        "Biomedical engineering allows me to combine this love of both whilst constantly" +
+                                        "challenging me mentally.")
+                                    {
+                                        Next = new DialogFrame(me, "That's so awesome, I might like the visual side too!")
+                                        {
+                                            Next = new DialogFrame(npc,
+                                                "It sure is. Come back and talk to me anytime.")
+                                        }
+                                    }
+                                }
+                            },
+                            {
+                                "Your experience?", new DialogFrame(npc,
+                                    "I was scared because I didn't have " +
+                                    "any coding, or really much " +
+                                    "mechanical knowledge that I would struggle.")
+                                {
+                                    Next = new DialogFrame(npc,
+                                        "But the first year courses were very approachable. " +
+                                        "The lecturers really do their best to help you get the skills you need and are" +
+                                        "always keen to give you their extra time.")
+                                    {
+                                        Next = new DialogFrame(me,
+                                            "Oh wow, that's reassuring!")
+                                        {
+                                            Next = new DialogFrame(npc,
+                                                "No problem, come to talk me anytime!")
+                                        }                                
+                                    }
+                                }
+                            },
+                            {
+                                "Any advice?", new DialogFrame(npc,
+                                    "Talk to as many second years as you can!")
+                                {
+                                    Next = new DialogFrame(npc,
+                                        " While the courses may not offer the best representation" +
+                                        " of the specialisation, those that have done it will tell you how it is.")
+                                    {
+                                        Next = new DialogFrame(me, "Thank you, that's really helpful!")
+                                        {
+                                            Next = new DialogFrame(npc,
+                                                "Thanks for talking to me, come back anytime!")
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        };
+
+        var directions = new Dictionary<string, DialogPosition>
+        {
+            { me, DialogPosition.Left }, { npc, DialogPosition.Right }
+        };
+
+        return new Dialog(frame, directions, () =>
+        {
+            Toolbox.Instance.QuestManager.MarkCurrentFinished("networking");
+            Toolbox.Instance.AchievementsManager.MarkCompleted("speak-undergrad");
+        });
+    }
+
+    public override void Update()
+    {
+        var networking = Toolbox.Instance.QuestManager.HasFinishedOrIsCurrent("networking");
+        gameObject.SetActive(networking);
+
+        base.Update();
+    }
+}
