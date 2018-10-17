c1f79edee23dcb2dd91255b31a3af2ba73fa4421
Franklin Wang <fractalemagic@gmail.com>
2018-10-17 19:32:09 +1300

    Programatically setup menu buttons & return to previous scene

---
+++ SOFTENG-306-Project-2/Assets/MenuController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class MenuController : MonoBehaviour
+{
+    void Start()
+    {
+        var play = transform.Find("Play").GetComponent<Button>();
+        var hiscores = transform.Find("Highscores").GetComponent<Button>();
+        var quit = transform.Find("Leave Game").GetComponent<Button>();
+
+        var scene = Toolbox.Instance.GameManager.Player.Scene ?? "Engineering Lobby";
+
+        // Setup buttons
+        play.onClick.AddListener(() => { Toolbox.Instance.GameManager.ChangeScene(scene); });
+        hiscores.onClick.AddListener(() => { Toolbox.Instance.GameManager.ChangeScene("Hiscores"); });
+        quit.onClick.AddListener(() => { Toolbox.Instance.GameManager.QuitGame(); });
+    }
+}
