6209ddd78d56948fc0c894f832dfbe9a2c919066
Tina Chen <tche278@aucklanduni.ac.nz>
2018-10-17 17:20:13 +1300

    Comment Electrical_Level script

--- SOFTENG-306-Project-2/Assets/Scripts/ElectricalScripts/Electrical_Level.cs
+++ SOFTENG-306-Project-2/Assets/Scripts/ElectricalScripts/Electrical_Level.cs
@@ -8,29 +8,33 @@ using System;
 public class Electrical_Level : MonoBehaviour, IDropZone
 {
     public Sprite newSprite;
-    private Draggable currentItem;
     public Draggable[] expectedGates;
     public Canvas endOfLevelCanvas;
+    private Draggable currentItem;
+
     public int levelNumber;
     public int TimeLimit = 10;
+    private int tutorialLevelNumber = 4;
+    private int levelScore = 100;
 
     private string PlayerName = "Anonymous";
     private TextMeshProUGUI timerArea;
     private bool timerNotStopped = true;
-    private float currCountdownValueTenthSeconds;
-    private float pausedValue;
     private bool paused = false;
     private bool expected = false;
     private bool secondDropZoneComplete = false;
     private bool levelCompleteCalled = false;
-    private int tutorialLevelNumber = 4;
-    private int levelScore = 100;
+    private float currCountdownValueTenthSeconds;
+    private float pausedValue;
 
-
+    /*
+     * Start called at beginning of level, initialises the rigidbodies in the
+     * scene and player name, and begins countdown
+     */
     public void Start()
     {
         var rigidbody = GetComponent<Rigidbody2D>();
-        if (rigidbody == null) 
+        if (rigidbody == null)
         {
             rigidbody = gameObject.AddComponent<Rigidbody2D>();
             rigidbody.bodyType = RigidbodyType2D.Kinematic;
@@ -41,7 +45,9 @@ public class Electrical_Level : MonoBehaviour, IDropZone
         SetPlayerName(Toolbox.Instance.GameManager.Player.Username);
     }
 
-    // Update is called once per frame
+    /*
+     * Update called once per frame
+     */
     void Update()
     {
         // check that a second drop zone exists
@@ -56,46 +62,53 @@ public class Electrical_Level : MonoBehaviour, IDropZone
             secondDropZoneComplete = GameObject.FindWithTag("DropZone2").GetComponent<Second_DropZone>().GetExpected();
         }
 
+        // if the the circuit is in the correct state
         if (expected && secondDropZoneComplete)
         {
+            // if level complete has not been called
             if (!levelCompleteCalled)
             {
                 levelCompleteCalled = true;
                 completeLevel();
             }
-            
+
         }
         // get paused value as the current countdown time
         pausedValue = currCountdownValueTenthSeconds;
 
         // check if the timer is deactivated
-        if (timerArea.color == Color.gray) {
+        if (timerArea.color == Color.gray)
+        {
             timerNotStopped = false;
             paused = true;
 
             // stop all current timing
             StopAllCoroutines();
 
-        } else if (timerArea.color == Color.black) {
+        }
+        else if (timerArea.color == Color.black)
+        {
             // check that the player is exiting a tutorial
-            if (paused == true) {
+            if (paused == true)
+            {
                 paused = false;
                 timerNotStopped = true;
 
                 // start new timing using current timer value on new background thread
-                StartCoroutine(StartCountdown(Mathf.RoundToInt(1+ (pausedValue/10))));
+                StartCoroutine(StartCountdown(Mathf.RoundToInt(1 + (pausedValue / 10))));
             }
         }
     }
 
+
     public void OnDragEnter(Draggable item)
     {
-       // GetComponent<SpriteRenderer>().color = new Color(1.0f, 0.0f, 0.0f);
+
     }
 
     public void OnDragExit(Draggable item)
     {
-     //   GetComponent<SpriteRenderer>().color = new Color(1.0f, 1.0f, 1.0f);
+
     }
 
     public void OnDragFinish(Draggable item)
@@ -108,7 +121,7 @@ public class Electrical_Level : MonoBehaviour, IDropZone
 
     public void OnDragStart(Draggable item)
     {
-        
+
     }
 
     public void OnDrop(Draggable item)
@@ -136,19 +149,20 @@ public class Electrical_Level : MonoBehaviour, IDropZone
 
     /*
      * Method that is called when the ciruit is completed correctly
-     */ 
+     */
     private void completeLevel()
     {
-        //GetComponent<SpriteRenderer>().color = new Color(1.0f, 1.0f, 1.0f);
+        // find the light and circuit pieces in the level board
         GameObject.FindWithTag("Light").GetComponent<SpriteRenderer>().sprite = newSprite;
-
         GameObject[] circuitPieces = GameObject.FindGameObjectsWithTag("Circuit");
 
+        // colour the circuit pieces yellow
         foreach (GameObject circ in circuitPieces)
         {
             circ.GetComponent<SpriteRenderer>().color = Color.yellow;
         }
 
+        // delay the time until the the end of level canvas is shown
         timerNotStopped = false;
         StartCoroutine(endOfLevel((delayTime) =>
         {
@@ -162,15 +176,19 @@ public class Electrical_Level : MonoBehaviour, IDropZone
         int timeRemain = Mathf.RoundToInt(currCountdownValueTenthSeconds / 10);
 
         // if time remaining is less than 50% of time limit
-        if (timeRemain < TimeLimit/2) {
+        if (timeRemain < TimeLimit / 2)
+        {
             // reduce the player score
-            levelScore = 99 - (TimeLimit/2 - timeRemain);
+            levelScore = 99 - (TimeLimit / 2 - timeRemain);
         }
         // set the level score in the end level dialog
         SetLevelScoreText(levelScore);
         Toolbox.Instance.Electrical_Scores.addScore(levelNumber, levelScore);
     }
 
+    /*
+     * Adds the scores from skipped levels to the high score board
+     */
     public void skipLevels()
     {
         for (int i = 1; i <= tutorialLevelNumber; i++)
@@ -183,13 +201,11 @@ public class Electrical_Level : MonoBehaviour, IDropZone
     {
         yield return new WaitForSeconds(2);
         callback(true);
-       
- 
     }
 
     public void OnItemDrag(Draggable item)
     {
-        
+
     }
 
     public void OnItemRemove(Draggable item)
@@ -203,13 +219,16 @@ public class Electrical_Level : MonoBehaviour, IDropZone
     }
 
     private void Awake()
-
     {
+        // find the timer area and set the label to the countdown
         timerArea = GameObject.Find("Timer").GetComponent<TextMeshProUGUI>();
         string timerLabel = String.Format("{0:00}:00", (TimeLimit));
         timerArea.SetText(timerLabel);
     }
 
+    /*
+     * Set the timer to the amount of time specified
+     */
     private void SetTimeAndAmount(int timeInSeconds, int amount)
     {
         TextMeshProUGUI resultInfoArea = GameObject.Find("ResultInfo").GetComponent<TextMeshProUGUI>();
@@ -224,12 +243,12 @@ public class Electrical_Level : MonoBehaviour, IDropZone
 
     /*
      * Set the player name in end level dialog
-     */ 
+     */
     private void SetPlayerName(string name)
     {
         string[] firstName = name.Split(null);
         TextMeshProUGUI nameArea = GameObject.Find("PlayerName").GetComponent<TextMeshProUGUI>();
-        nameArea.SetText(firstName[0]+"!");
+        nameArea.SetText(firstName[0] + "!");
     }
 
     /*
@@ -241,6 +260,9 @@ public class Electrical_Level : MonoBehaviour, IDropZone
         nameArea.SetText(score + "");
     }
 
+    /*
+     * Begin the timer and refresh the timer text every 0.1 seconds
+     */
     public IEnumerator StartCountdown(int timeLimit)
     {
         float countdownValue = (timeLimit - 1) * 10;
