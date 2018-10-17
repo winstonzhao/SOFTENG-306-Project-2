d10a8734f40dfd0fbcc56833999d413dfc2ed67a
rmberriman <rjl@hotmail.co.nz>
2018-10-17 20:14:03 +1300

    Bug fixing2
    Co-Authored-By: twchen97 <twchen97@users.noreply.github.com>

--- SOFTENG-306-Project-2/Assets/Scripts/ElectricalScripts/Second_DropZone.cs
+++ SOFTENG-306-Project-2/Assets/Scripts/ElectricalScripts/Second_DropZone.cs
@@ -79,28 +79,34 @@ public class Second_DropZone : MonoBehaviour, IDropZone
      */
     public void OnDrop(Draggable item)
     {
-        currentItem = item;
-        currentItem.HomePos = transform.position;
-
-        // check for each logic gate
-        foreach (Draggable gate in expectedGates)
+        if (currentItem == null)
         {
-            // check if a logic gate is expected in dropzone
-            if (currentItem == gate)
-            {
-                expected = true;
-            }
-        }
+            currentItem = item;
+            currentItem.HomePos = transform.position;
 
-        // if logic gate is expected, turn the corresponding circuit pieces yellow
-        if (expected)
+            // check for each logic gate
+            foreach (Draggable gate in expectedGates)
+            {
+                // check if a logic gate is expected in dropzone
+                if (currentItem == gate)
+                {
+                    expected = true;
+                }
+            }
+
+            // if logic gate is expected, turn the corresponding circuit pieces yellow
+            if (expected)
+            {
+                GameObject[] circuitPieces = GameObject.FindGameObjectsWithTag(circuitTag);
+
+                foreach (GameObject circ in circuitPieces)
+                {
+                    circ.GetComponent<SpriteRenderer>().color = Color.yellow;
+                }
+            }
+        } else
         {
-            GameObject[] circuitPieces = GameObject.FindGameObjectsWithTag(circuitTag);
 
-            foreach (GameObject circ in circuitPieces)
-            {
-                circ.GetComponent<SpriteRenderer>().color = Color.yellow;
-            }
         }
 
     }
